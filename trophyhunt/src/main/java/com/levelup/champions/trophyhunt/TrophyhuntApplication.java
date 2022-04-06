53fb5d50bc993930483c373dcc79ef8367a728af
Brent Hoch <brent.hoch@gmail.com>
2022-04-06 16:45:05 -0400

    setup of environment

---
+++ trophyhunt/src/main/java/com/levelup/champions/trophyhunt/TrophyhuntApplication.java
@@ -0,0 +1,14 @@
+package com.levelup.champions.trophyhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrophyhuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrophyhuntApplication.class, args);
+		System.out.println("helloooooooooooooooooooooooooooo woooorld");
+	}
+
+}
