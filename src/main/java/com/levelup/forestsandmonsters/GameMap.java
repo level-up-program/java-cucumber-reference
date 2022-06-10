58b0ea7f243ccda03c538e3ec21af520a31fdc2a
Erin Perry <georeri@iit.edu>
2022-06-10 11:43:50 -0700

    Map needs refactoring but works

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,5 +1,8 @@
 package com.levelup.forestsandmonsters;
 
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+import java.awt.Point;
+
 public class GameMap {
 
     Position[][] positions;
@@ -18,6 +21,40 @@ public class GameMap {
 
     public Position[][] getPositions() {
         return positions;
+    }
+
+    public Position calculatePosition(Position startingPosition, DIRECTION direction) {
+        int calculatedX = startingPosition.coordinates.x;
+        int calculatedY = startingPosition.coordinates.y;
+
+        if(direction.equals(DIRECTION.NORTH)){
+            calculatedY = calculatedY + 1;
+        }
+        if(direction.equals(DIRECTION.SOUTH)){
+            calculatedY = calculatedY - 1;
+        }
+        if (direction.equals(DIRECTION.WEST)) {
+            calculatedX = calculatedX - 1;
+        }
+        if (direction.equals(DIRECTION.EAST)) {
+            calculatedX = calculatedX + 1;
+        }
+
+        if(isPositionValid(new Point(calculatedX, calculatedY))){
+            return this.positions[calculatedX][calculatedY];
+        }
+        else {
+            return startingPosition;
+        }
+    }
+
+    boolean isPositionValid(Point positionCoordinates) {
+        if(positionCoordinates.x < 0 || positionCoordinates.x > 9)
+            return false;
+        else if (positionCoordinates.y < 0 || positionCoordinates.y > 9)
+            return false;
+        else 
+            return true;
     }
 
 }
