12efa9e8b9a56227d3383ae9073d4d699ec2b953
Erin Perry <georeri@iit.edu>
2022-07-28 16:57:33 -0400

    Typo update

--- src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 
 import com.levelup.forestsandmonsters.GameController.DIRECTION;
 
-import net.bytebuddy.dynamic.scaffold.MethodGraph.Compiler.Default.Merger.Directional;
-
 public class GameControllerTest {
     @Test
     public void initializationCreatesResults() {
